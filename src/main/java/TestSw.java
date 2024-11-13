ede9c233a5f388c2a9f91d842c69486f551ca585
yangsong3 <yangsong9988@gmail.com>
2024-11-13 10:24:41 +0900

    JCheckBox

--- src/main/java/TestSw.java
+++ src/main/java/TestSw.java
@@ -14,7 +14,7 @@ public class TestSw extends JFrame {
         Container cp = getContentPane(); // 프레임에서 컨텐트 팬 받아오기
 
         //Layout
-        cp.setLayout(null);
+        cp.setLayout(new FlowLayout());
 
         //JLabel
         JLabel jLabel = new JLabel("Label Test");
@@ -31,6 +31,12 @@ public class TestSw extends JFrame {
         jTextField.setBounds(200, 200, 100, 25);
         jTextField.setColumns(10);
         cp.add(jTextField);
+
+        //JCheckBox
+        JCheckBox lion = new JCheckBox("lion");
+        JCheckBox rabbit = new JCheckBox("rabbit", true);
+        cp.add(lion);
+        cp.add(rabbit);
 
 
 
